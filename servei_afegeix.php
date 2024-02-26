e5aa6c6cb889ff023347e1ffb601adf6546bfbe7
urii08 <omolina2@ies-eugeni.cat>
2024-02-26 20:36:16 +0100

    commit big boss

--- servei_afegeix.php
+++ servei_afegeix.php
@@ -5,12 +5,15 @@
 </head>
 <body>
     <h2>Afegeix un producte</h2>
-    <form action="servei_afegeix_id.php" method="POST">
+    <form action="servei_afegeix_id.php" method="POST" enctype="multipart/form-data">
         <label for="name">Nom del producte:</label><br>
         <input type="text" id="name" name="name"><br>
         <label for="description">Descripció:</label><br>
-        <textarea id="description" name="description"></textarea><br><br>
+        <textarea id="description" name="description"></textarea><br>
+        <label for="image">Imatge:</label><br>
+        <input type="file" id="image" name="image"><br><br>
         <input type="submit" value="Afegir producte">
     </form>
 </body>
 </html>
+
