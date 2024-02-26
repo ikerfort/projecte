887c819006c8add2e4388614958c707ec0b939f3
smolina21 <smolina@ies-eugeni.cat>
2024-02-26 19:40:40 +0100

    about

--- about.php
+++ about.php
@@ -19,9 +19,7 @@
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+            Amb dedicació incansable i compromís amb l'excel·lència, el nostre equip de treballadors vetlla per oferir-vos productes informàtics i perifèrics de darrera generació. Mantenim un estàndard d'actualització constant, garantint la màxima qualitat al millor preu. El nostre esforç es tradueix en la satisfacció dels nostres clients.
             </p>
             <a href="#" class="btn-learn-more">Learn More</a>
           </div>
