f32543968ea1306843443b860b1f4097346f3832
marc <mmontserr6@ies-eugeni.cat>
2024-02-26 19:19:35 +0100

    commit2 serveis

--- serveis.php
+++ serveis.php
@@ -8,9 +8,9 @@
     </div>
 
     <div class="row justify-content-center"> 
-      <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="100">
+      <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
         <div class="icon-box text-center"> 
-          <div class="icon"><i class="bx bxl-dribbble"></i></div>
+          <div class="icon"><i class="bx bx-rotate-right"></i></div>
           <h4><a href="">Actualitzar</a></h4>
           <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
         </div>
@@ -18,15 +18,15 @@
 
       <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
         <div class="icon-box text-center">
-          <div class="icon"><i class="bx bx-file"></i></div>
+          <div class="icon"><i class="bx bx-xmark"></i></div>
           <h4><a href="">Esborrar</a></h4>
           <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
         </div>
       </div>
-<!-- houuh -->
+
       <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
         <div class="icon-box text-center">
-          <div class="icon"><i class="bx bx-tachometer"></i></div>
+          <div class="icon"><i class="bx bx-plus"></i></div>
           <h4><a href="">Afegir</a></h4>
           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
         </div>
