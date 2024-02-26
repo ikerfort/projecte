bc3c0367a85662b17e54e936a3d465d38fd7fc1a
smolina21 <smolina@ies-eugeni.cat>
2024-02-26 19:57:32 +0100

    about

--- hero.php
+++ hero.php
@@ -7,7 +7,7 @@
       <h1>¡Els millors productes i periférics!</h1>
       <h2>Tecnologia actualitzada al millor preu: la nostra passió, la teva solució.</h2>
       <div class="d-flex justify-content-center justify-content-lg-start">
-        <a href="#about.php" class="btn-get-started scrollto">Get Started</a>
+        <a href="#about" class="btn-get-started scrollto">Som-hi</a>
         <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
       </div>
     </div>
