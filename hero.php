d6d9bbe55ac65412505f2224e445233a26fa71cb
urii08 <omolina2@ies-eugeni.cat>
2024-02-26 20:00:43 +0100

    final hero

--- hero.php
+++ hero.php
@@ -8,7 +8,6 @@
       <h2>Tecnologia actualitzada al millor preu: la nostra passió, la teva solució.</h2>
       <div class="d-flex justify-content-center justify-content-lg-start">
         <a href="#about" class="btn-get-started scrollto">Som-hi</a>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
       </div>
     </div>
     <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
