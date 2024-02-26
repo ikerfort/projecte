0b5060b26f99900e9eff4921458fcaad691ede47
marc <mmontserr6@ies-eugeni.cat>
2024-02-26 20:30:17 +0100

    commit 7 serveis

--- servei_actualitzar_id.php
+++ servei_actualitzar_id.php
@@ -1,27 +1,26 @@
 <?php
 include('connexio_woo.php');
 
-// Verifiquem si s'ha enviat un identificador de producte
 if (isset($_POST['product_id'])) {
     $product_id = $_POST['product_id'];
 
-    // Obtenim la informació del producte seleccionat
     $product = $woocommerce->get('products/' . $product_id);
     
-    // Mostrem el formulari amb els paràmetres actuals del producte
     ?>
     <form action="actualitzar_producte.php" method="post">
         <input type="hidden" name="product_id" value="<?php echo $product->id; ?>">
 
         <label for="product_name">Nom del producte:</label>
-        <input type="text" id="product_name" name="product_name" value="<?php echo $product->name; ?>" required>
+        <input type="text" id="product_name" name="product_name" value="<?php echo $product->name; ?>" required> <br><br>
 
         <label for="product_price">Preu del producte:</label>
-        <input type="number" id="product_price" name="product_price" value="<?php echo $product->price; ?>" required>
+        <input type="number" id="product_price" name="product_price" value="<?php echo $product->price; ?>" required> <br><br>
 
-        <!-- Afegiu altres paràmetres del producte aquí -->
+        <label for="product_description">Descripció del producte:</label>
+        <input type="text" id="product_description" name="product_description" value="<?php echo $product->description; ?>" required>
 
-        <input type="submit" value="Actualitzar producte">
+
+        <br><br><input type="submit" value="Actualitzar producte">
     </form>
     <?php
 } else {
