985e53eecb2fea56c0de8156122a4f0375b87220
urii08 <omolina2@ies-eugeni.cat>
2024-02-26 20:39:20 +0100

    fina lcommit

--- servei_afegeix.php
+++ servei_afegeix.php
@@ -7,11 +7,11 @@
     <h2>Afegeix un producte</h2>
     <form action="servei_afegeix_id.php" method="POST" enctype="multipart/form-data">
         <label for="name">Nom del producte:</label><br>
-        <input type="text" id="name" name="name"><br>
+        <input type="text" id="name" name="name"><br><br>
         <label for="description">Descripció:</label><br>
-        <textarea id="description" name="description"></textarea><br>
-        <label for="image">Imatge:</label><br>
-        <input type="file" id="image" name="image"><br><br>
+        <textarea id="description" name="description"></textarea><br><br>
+        <label for="image">Imatge:</label>
+        <input type="file" id="image" name="image"><br><br>&nbsp;
         <input type="submit" value="Afegir producte">
     </form>
 </body>
